da28903eadb3ca55d3e94a6631d4ff35aa10c4c5
CannibalChoco <fudgeblimp@gmail.com>
2018-04-04 11:17:20 +0300

    feat: display all body parts in master list fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.android_me.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class MasterListFragment extends Fragment {
+
+    public MasterListFragment (){
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        Context context = getContext();
+
+        MasterListAdapter adapter = new MasterListAdapter(context, AndroidImageAssets.getAll());
+        GridView gridview = rootView.findViewById(R.id.master_list_gridView);
+
+        gridview.setAdapter(adapter);
+
+        return rootView;
+    }
+}
